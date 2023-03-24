59d63a922fffb65c1a823483321890454b58de5a
Inna Nomerovkina <inkosof@gmail.com>
2023-03-24 12:35:43 +0200

    task-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryEl = document.querySelector(".gallery");
+const galleryMarkUp = createGalleryItems(galleryItems);
+
+galleryEl.insertAdjacentHTML('beforeend', galleryMarkUp);
+
+function createGalleryItems(galleryItems) {
+    return galleryItems.map(
+        ({ preview, original, description }) => {
+            return `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`;
+        }
+    ).join("");
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: '250',
+
+ });
