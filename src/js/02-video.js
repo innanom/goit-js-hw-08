4ab669f4623ee3c37c677232fc83307e03b46d3d
Inna Nomerovkina <inkosof@gmail.com>
2023-03-26 00:52:07 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,14 +2,18 @@ import throttle from 'lodash.throttle'
 import Player from '@vimeo/player'
 
     const iframe = document.querySelector('iframe');
-    const player = new Player(iframe);
+const player = new Player(iframe);
+const CURRENT_TIME_KEY = "videoplayer-current-time";
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
+    player.on('timeupdate', throttle(onPlay, 500));
 
-    player.getVideoTitle().then(function(title) {
-        console.log('title:', title);
-    });
+function onPlay(data) {
+        localStorage.setItem(CURRENT_TIME_KEY, data.seconds)
+    }
+
+if (localStorage.getItem(CURRENT_TIME_KEY)) {
+    player.setCurrentTime(localStorage.getItem(CURRENT_TIME_KEY))
+}
+
 
 
